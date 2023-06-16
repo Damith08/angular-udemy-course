0033f3776c1a8220f7c706d0b45196951f133e69
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-06-16 16:23:54 +0530

    feat: add new component
    add new component to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  //styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      h3 {
+        color: red;
+      }
+    `,
+  ],
 })
-export class AppComponent {}
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+}
