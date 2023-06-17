6af7f89decb33711250f5af249c99eed9da57c65
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-06-17 15:58:57 +0530

    feat: two way binding
    two way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = '';
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
@@ -23,7 +23,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created!' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
